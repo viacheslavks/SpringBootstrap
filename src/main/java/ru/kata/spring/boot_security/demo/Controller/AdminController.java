8a0abfa750ba30595ee57e8e3ad8a5ed36d4c0c4
Viacheslav.Kiselev <Viacheslav.kiselev@gmail.com>
2024-03-16 21:47:23 +0300

    first modal

--- src/main/java/ru/kata/spring/boot_security/demo/Controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/Controller/AdminController.java
@@ -40,13 +40,6 @@ public class AdminController {
         return "adminList";
 
     }
-//
-//    @GetMapping(value = "/addForm")
-//    public String addForm(@ModelAttribute("user") User user, Model model) {
-//        List<Role> roles = roleService.getAllRoles();
-//        model.addAttribute("allRoles", roles);
-//        return "addForm";
-//    }
 
     @PostMapping(value = "/addForm")
     public String saveUser(@ModelAttribute("user") User user, @RequestParam("selectedRoles") List<Long> selectResult) {
@@ -60,28 +53,30 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    @GetMapping(value = "/editForm")
-    public String editForm(@RequestParam("userId") Long id, Model model) {
-        model.addAttribute("user", userService.getUserById(id));
-        List<Role> roles = (List<Role>) roleService.getAllRoles();
-        model.addAttribute("allRoles", roles);
-        return "update";
-    }
+//    @GetMapping(value = "/editForm")
+//    public String editForm(@RequestParam("userId") Long id, Model model) {
+//        model.addAttribute("user", userService.getUserById(id));
+//        List<Role> roles = (List<Role>) roleService.getAllRoles();
+//        model.addAttribute("allRoles", roles);
+//        return "update";
+//    }
 
-    @PostMapping("/update")
-    public String updateUser(@ModelAttribute("user") User user, @RequestParam("selectedRoles") List<Long> selectResult) {
+    @PostMapping("/editUser")
+    public String editUser(@RequestParam("id") Long id, @RequestParam("selectedRoles") List<Long> selectResult, Model model) {
         List<Role> roles = new ArrayList<>();
         for (Long s : selectResult) {
             roles.add(roleService.getRoleById(s));
         }
-        user.setRoles(roles);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userService.update(user);
+        User userEdit = userService.getUserById(id);
+        model.addAttribute("user", userEdit);
+        userEdit.setRoles(roles);
+        userEdit.setPassword(passwordEncoder.encode(userEdit.getPassword()));
+        userService.update(userEdit);
         return "redirect:/admin";
     }
 
     @GetMapping(value = "/deleteUser")
-    public String deleteUser(@RequestParam("userId") Long id) {
+    public String deleteUser(@RequestParam("id") Long id) {
         userService.delete(id);
         return "redirect:/admin";
     }
